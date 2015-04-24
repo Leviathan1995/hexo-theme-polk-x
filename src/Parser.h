eef43ea2727b185be20fbab856e309342b2fe9c7
Leviathan1995 <Leviathan1995@outlook.com>
2015-04-24 11:27:36 +0800

    update

--- src/Parser.h
+++ src/Parser.h
@@ -1,6 +1,6 @@
 /*************************************************************************
 	> File Name: Parser.h
-	> Author: Leviathan1995
+    > Author: Leviathan1995
 	> Mail: Leviathan1995@outlook.com
 	> Created Time: 2015年04月24日 星期五 02时25分25秒
  ************************************************************************/
@@ -9,19 +9,25 @@
 #define _PARSER_H
 #include <string>
 #include <map>
+
 using namespace std;
+map<string,string> Http_HeadText;
+
 //Http_Server 头部
 struct Http_Header
 {
-    string Method;
-    string Url;
-    stirng Version;
+    string Method; //方法
+    string Url;    //链接
+    stirng Version;//版本
+    Http_HeadText Header;//首部
+    string body;
 }
 //Http_Server 类
 class Http_Server
 {
     public:
-    void Http_PrintServer()
+    void Http_PrintHttpHeadText(); //打印Http_HeadText
+    void Http_PrintHttpHead();     //打印Http_HeadText
 
 }
 #endif
