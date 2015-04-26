bc0bd7a09ffd2ee18ff88fa0e36c5bbaee492377
Leviathan1995 <Leviathan1995@outlook.com>
2015-04-27 01:11:01 +0800

    update

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -7,7 +7,8 @@
 
 #include<iostream>
 using namespace std;
-#include "Parser.cpp"
+#include "Parser.h"
+#include "string"
 //构造函数
 Http_Server::Http_Server()
 {
@@ -19,19 +20,65 @@ Http_Server::~Http_Server()
     
 }
 //打印Http_HeadText
-void Http_Server:: Http_PrintHttpHeadText()
+void Http_Server:: Http_PrintHttpHeadText(Http_HeadText & head)
 {
-    
+    if(!head.empty())
+    {
+        for(auto i=head.begin();i!=head.end();i++)
+            cout<<i->first<<":"<<i->second<<endl;
+    }
 }
 //打印Http_Header
-void Http_Server::Http_PrintHttpHead()
+void Http_Server::Http_PrintHttpHead(Http_Header &head)
 {
-    
+    if(head==NULL)
+    {
+        cout<<"Http_Header == NULL"<<endl;
+        return ;
+    }
+    cout<<head->Method<<" "<<head->Url<<" "<<head->Version<<endl;
+    Http_PrintHttpHeadText(head->Header);
+    cout<<endl;
+    cout<<head->Body<<endl;
 }
 //解析http request
-void Http_Server::Http_ParserRequest(Http_Header * http_header)
+bool  Http_Server::Http_ParserRequest(string & request,Http_Header * http_header)
 {
-    
+    if(request.empty())
+    {
+        cout<<" Http_ReturnRequest: http_request is null"<<endl;
+        return false;
+    }
+    if(http_header==NULL)
+    {
+        cout<<" Http_ReturnRequest: http_header is null"<<end;
+        return false;
+    }
+    string Crlf("\r\n"),Crlf2("\r\n\r\n");
+    int Index=0,Next=0;
+
+    //解析Http请求的起始行
+    if(Next=http_request.find(Crlf,Index)==string::nops)
+    {
+        string FIrst_line(http_request.substr(Index,Next-Index));
+        Index=Next;
+        stringstream sstream(FIrst_line);
+        sstream>>(head->Method);
+        sstream>>(head->Url);
+        sstream>>(head->Version);
+    }
+    else{
+        cout<<"Parser_Request:http_request is not have \\r\\n";
+        return false;
+    }
+    //查找\r\n\r\n
+    Index=http_request.find(Crlf2,Index);
+    if(Index==string::npos)
+    {
+        cout<<"Parser_Request:http_request have not a \"\r\n\r\n\"";
+        return false;
+    }
+
 }
 //返回指定的用户请求
 bool Http_Server::Http_ReturnRequest(string & http_request,Http_Header *http_header)
--- src/Parser.h
+++ src/Parser.h
@@ -1,13 +1,14 @@
 /*************************************************************************
-	> File Name: Parser.h
+    > File Name: Parser.h
     > Author: Leviathan1995
-	> Mail: Leviathan1995@outlook.com
-	> Created Time: 2015年04月24日 星期五 02时25分25秒
+    > Mail: Leviathan1995@outlook.com
+    > Created Time: 2015年04月24日 星期五 02时25分25秒
  ************************************************************************/
 #pragma once
 #ifndef _PARSER_H
 #define _PARSER_H
 #include <string>
+#include <sstream>
 #include <map>
 
 using namespace std;
@@ -20,7 +21,7 @@ struct Http_Header
     string Url;    //链接
     stirng Version;//版本
     Http_HeadText Header;//首部
-    string body;
+    string Body;
 }
 //Http_Server 类
 class Http_Server
@@ -28,11 +29,9 @@ class Http_Server
     public:
     Http_Server();
     ~Http_Server();
-    void Http_PrintHttpHeadText(); //打印Http_HeadText
-    void Http_PrintHttpHead();     //打印Http_Header
-    void Http_ParserRequest(Http_Header * http_header);     //解析http_request
-    bool Http_ReturnRequest(string & http_request,Http_Header *http_header);//返回指定的用户的请求
-
-
+    void Http_PrintHttpHeadText(Http_HeadText & head); //打印Http_HeadText
+    void Http_PrintHttpHead(Http_Header & head);            //打印Http_Header
+    bool  Http_ParserRequest(string & request,Http_Header * http_header);     //解析http_request
+    string Http_ReturnRequest(string & key,Http_Header & header);//返回指定的用户的请求
 }
 #endif
