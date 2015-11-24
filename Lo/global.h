278e1d49b7c7892af75b8c308fba5ad601e20301
Leviathan <Leviathan1995@outlook.com>
2015-11-24 16:21:56 +0800

    Create global.h

---
+++ Lo/global.h
@@ -0,0 +1,22 @@
+//
+//  global.h
+//  Lo
+//
+//  Created by Leviathan on 15/11/22.
+//  Copyright © 2015年 Leviathan. All rights reserved.
+//
+
+#ifndef global_h
+#define global_h
+
+#include <sys/epoll.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string.h>
+
+#define MAX_BACKLOG 100 //监听队列最大数
+#define MAX_EVENTS 1024 //epoll最大监听事件数
+
+
+#endif /* global_h */
