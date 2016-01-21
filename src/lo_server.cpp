61360b68d0edd7d9d4a7ee5e49cfad8e0a12e46e
Leviathan <Leviathan1995@outlook.com>
2016-01-21 02:04:49 -0800

    Update lo_server.cpp

--- src/lo_server.cpp
+++ src/lo_server.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <uv.h>
-#include <string>
+#include <assert.h>
+#include <string.h>
 #include "lo_server.h"
 using namespace std;
 
+uv_tcp_t _server;
+uv_tcp_t _client;
+uv_loop_t * _loop;
+const char* http_respone = "HTTP/1.1 200 OK\r\n"
+    "Content-Type:text/html;charset=utf-8\r\n"
+        "Content-Length:18\r\n"
+            "\r\n"
+                "Welcome to tinyweb";
+
 void lo::tinyweb_start(uv_loop_t* loop, const char* ip, int port) {
-        struct sockaddr_in addr;
-        uv_ip4_addr((ip && ip[0]) ? ip : "0.0.0.0", port, &addr);
+        sockaddr_in addr;
+        uv_ip4_addr(ip, port, &addr);
         _loop = loop;
         uv_tcp_init(_loop, &_server);
         uv_tcp_bind(&_server, (const struct sockaddr*) &addr, 0);
@@ -14,32 +24,27 @@ void lo::tinyweb_start(uv_loop_t* loop, const char* ip, int port) {
 }
 
 void lo::after_uv_close(uv_handle_t* handle) {
-        free(handle); // uv_tcp_t* client, see tinyweb_on_connection()
+        delete handle; // uv_tcp_t* client, see tinyweb_on_connection()
 }
 
 void lo::after_uv_write(uv_write_t* w, int status) {
         if(w->data)
-            free(w->data);
+            delete w->data;
         uv_close((uv_handle_t*)w->handle, after_uv_close); // close client
-        free(w);
+        delete w;
 
 }
 
 
-void lo::write_uv_data(uv_stream_t* stream, const void* data, unsigned int len, int need_copy_data) {
+void lo::write_uv_data(uv_stream_t* stream, const char* data, unsigned int len, int need_copy_data) {
         uv_buf_t buf;
-        uv_write_t* w;
-        void* newdata  = (void*)data;
+        uv_write_t* w=new uv_write_t;
+        char* newdata  =strdup(data);
         if(data == NULL || len == 0) return;
         if(len ==(unsigned int)-1)
             len = strlen(data);
-        if(need_copy_data) {
-            newdata = malloc(len);
-            memcpy(newdata, data, len);
-        }
         buf = uv_buf_init(newdata, len);
-        w = (uv_write_t*)malloc(sizeof(uv_write_t));
-        w->data = need_copy_data ? newdata : NULL;
+        w->data = newdata;
         uv_write(w, stream, &buf, 1, after_uv_write); // free w and w->data in after_uv_write()
 
 }
@@ -47,7 +52,7 @@ void lo::write_uv_data(uv_stream_t* stream, const void* data, unsigned int len, 
 void lo::tinyweb_on_connection(uv_stream_t* server, int status) {
     assert(server == (uv_stream_t*)&_server);
     if(status == 0) {
-        uv_tcp_t* client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
+        uv_tcp_t* client = new uv_tcp_t;
         uv_tcp_init(_loop, client);
         uv_accept((uv_stream_t*)&_server, (uv_stream_t*)client);
         write_uv_data((uv_stream_t*)client, http_respone, -1, 0);
