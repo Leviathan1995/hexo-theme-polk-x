845ec0779815a482b0ebb17cc00e902498ef2ab2
leviathan1995 <leviathan1995@outlook.com>
2016-01-23 11:11:08 -0800

    fix bug

--- src/lo_server.cpp
+++ src/lo_server.cpp
@@ -40,8 +40,6 @@ void lo::on_uv_alloc(uv_handle_t * handle,size_t suggested_size,uv_buf_t *buf)
 
 void lo::after_uv_write(uv_write_t* w, int status) {
         uv_close((uv_handle_t*)w->handle, after_uv_close); // close client
-        delete w;
-
 }
 
 void lo::on_uv_read(uv_stream_t *client,ssize_t nread,const uv_buf_t *buf)
@@ -55,10 +53,7 @@ void lo::on_uv_read(uv_stream_t *client,ssize_t nread,const uv_buf_t *buf)
 	{
 		tinyweb_close_client(client);
 	}
-        if(buf->base)
-            delete buf;
 }
-
 void lo::write_uv_data(uv_stream_t* stream, const char* data, unsigned int len, int need_copy_data) {
         uv_buf_t buf;
         uv_write_t* w=new uv_write_t;
