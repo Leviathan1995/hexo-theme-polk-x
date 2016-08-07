ae2d59d7bf1c7c67d317764ba6cfbf92418b4223
Leviathan <Leviathan1995@outlook.com>
2016-08-07 19:46:02 +0800

    Create http.h

---
+++ src/http.h
@@ -0,0 +1,64 @@
+//
+// Created by leviathan on 16/3/24.
+//
+
+#ifndef LO_HTTP_H
+#define LO_HTTP_H
+
+#include <iostream>
+#include <fstream>
+
+namespace lo
+{
+    
+    
+    //status code
+    #define HTTP_OK 200;
+    #define HTTP_NOT_FIND 404;
+    
+    //method
+    #define HTTP_GET 0x0001
+    #define HTTP_POST 0x0002
+    #define HTTP_HEAD 0x0003
+    
+    //version
+    #define HTTP_VERSION_1_1 0X0011;
+    
+    class http_request
+    {
+    public:
+        http_request(){};
+        ~http_request()
+        {
+            delete [] url;
+            delete [] host;
+        };
+        unsigned int    method;
+        unsigned int    version;
+        char *  url;
+        char *  host;
+    };
+    
+    class http_response
+    {
+    public:
+        int status;
+        unsigned int version;
+        std::string type;
+        std::string charset;
+        
+    };
+    class http
+    {
+    public:
+        http(){};
+        ~http(){};
+        static http_request request;
+        static http_response response;
+        static void  parse_request(const char * buff,int num);
+        static char * get_response();
+        
+        
+    };
+}//namespace lo
+#endif //LO_HTTP_H
